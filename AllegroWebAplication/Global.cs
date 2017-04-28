be21e18681d107af05a7c89edf6e17f9a22fae7b
Adam Jankowski <adam.jankowski@protonmail.com>
2017-04-28 12:39:16 +0200

    Basic setup

---
+++ AllegroWebAplication/Global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AllegroWebAplication
+{
+    public static class Global
+    {
+        public static string allegroApiKey = "";
+        public static string allegroLogin = "";
+        public static string allegroPassword = "";
+    }
+}
