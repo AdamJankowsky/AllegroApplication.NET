e295b2780a75665618a49dc7b96567008b44ff3d
Adam Jankowski <adam1jankowski@gmail.com>
2017-04-29 23:57:18 +0200

    Added Allegro Controller

---
+++ AllegroWebAplication/Controllers/AllegroController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AllegroWebAplication.Providers.Interfaces;
+using AllegroWebAplication.AllegroModels;
+
+namespace AllegroWebAplication.Controllers
+{
+    public class AllegroController : Controller
+    {
+        IAllegroApiWrapper wrapper;
+
+        public AllegroController(IAllegroApiWrapper _wrapper)
+        {
+            wrapper = _wrapper;
+        }
+        
+        // GET: Allegro
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+        public ActionResult ListBoughtItems()
+        {
+            var boughtItems = wrapper.GetMyWonItems();
+
+            return View(boughtItems);
+        }
+        public ActionResult ListSellingItems()
+        {
+            var sellingItems = wrapper.GetMySellItems();
+            return View(sellingItems);
+        }
+        public ActionResult ListListSoldItems()
+        {
+            var soldItems = wrapper.GetMySoldItems();
+            return View(soldItems);
+        }
+    }
+}
