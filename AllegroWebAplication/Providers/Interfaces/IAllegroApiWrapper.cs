9a7610eac4c4aee81412139879b2af4852a29ff8
Adam Jankowski <adam.jankowski@protonmail.com>
2017-04-29 14:24:51 +0200

    Added interfeace with basic functions.

---
+++ AllegroWebAplication/Providers/Interfaces/IAllegroApiWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AllegroWebAplication.AllegroModels;
+
+namespace AllegroWebAplication.Providers.Interfaces
+{
+    interface IAllegroApiWrapper
+    {
+        string DoLogin();
+        long GetLocalVersionKey();
+        AllegroSellItems GetMySellItems();
+        AllegroSoldItems GetMySoldItems();
+        AllegroWonItems GetMyWonItems();
+    }
+}
